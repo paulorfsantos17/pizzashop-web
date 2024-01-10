1fa9effdeec2462872cca062ffe4be33e2bd699c
Paulo <paulorfsantos17@gmail.com>
2024-01-10 20:48:14 -0300

    feat: add  component OrderTableFilters

--- src/pages/orders/components/order-table-filters.tsx
+++ src/pages/orders/components/order-table-filters.tsx
@@ -1,4 +1,14 @@
+import { Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 // interface IOrderTableFiltersProps = {}
 
 export function OrderTableFilters({}: IOrderTableFiltersProps) {
@@ -7,6 +17,27 @@ export function OrderTableFilters({}: IOrderTableFiltersProps) {
       <span className="text-sm font-semibold">Filtros:</span>
       <Input placeholder="ID do cliente" className="h-8 w-auto" />
       <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+      <Select>
+        <SelectTrigger className="h-8 w-[180px]">
+          <SelectValue></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">Todos</SelectItem>
+          <SelectItem value="pending">Pedente</SelectItem>
+          <SelectItem value="canceled">Cancelado</SelectItem>
+          <SelectItem value="processing">Em preparo</SelectItem>
+          <SelectItem value="delivering">Em entrega</SelectItem>
+          <SelectItem value="delivered">Entregue</SelectItem>
+        </SelectContent>
+      </Select>
+      <Button type="submit" variant="secondary" size="xs">
+        <Search className="mr-2 h-4 w-4"></Search>
+        Filtar Resultados
+      </Button>
+      <Button type="button" variant="outline" size="xs">
+        <X className="mr-2 h-4 w-4"></X>
+        Remover Filtros
+      </Button>
     </form>
   )
 }
