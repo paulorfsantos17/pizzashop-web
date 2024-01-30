bb9b0c0f71d437131794a7adaca2fb91e95c7191
Paulo <paulorfsantos17@gmail.com>
2024-01-30 19:34:58 -0300

    feat: ensure email register in signUp it is  send with params to page signIn

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -16,12 +16,14 @@ const signInForm = z.object({
 
 type SignInForm = z.infer<typeof signInForm>
 export function SignIn() {
+  const [searchParams] = useSearchParams()
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignInForm>({
     resolver: zodResolver(signInForm),
+    defaultValues: { email: searchParams.get('email') ?? '' },
   })
 
   const { mutateAsync: authenticate } = useMutation({
