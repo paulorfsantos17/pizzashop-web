f3c9748eb8f32d23df68db77ac7d00d10f3fea4c
Paulo <paulorfsantos17@gmail.com>
2024-02-01 18:10:54 -0300

    feat: ensure get orders api and show page orders

---
+++ src/api/get-orders.ts
@@ -0,0 +1,25 @@
+import { api } from '@/lib/axios'
+export interface IGetOrdersResponse {
+  orders: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }[]
+  meta: {
+    pageIndex: number
+    perPage: number
+    totalCount: number
+  }
+}
+
+export async function getOrders() {
+  const response = await api.get<IGetOrdersResponse>('/orders', {
+    params: {
+      pageIndex: 0,
+    },
+  })
+
+  return response.data
+}
---
+++ src/components/order-status.tsx
@@ -0,0 +1,37 @@
+type IOrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface IOrderStatusProps {
+  status: IOrderStatus
+}
+
+const orderStatusMap: Record<IOrderStatus, string> = {
+  pending: 'Pedente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em Preparo',
+}
+
+const orderStatusStyleMap: Record<IOrderStatus, string> = {
+  pending: 'h-2 w-2 rounded-full bg-slate-400',
+  canceled: 'h-2 w-2 rounded-full bg-rose-500',
+  delivered: 'h-2 w-2 rounded-full bg-emerald-500',
+  delivering: 'h-2 w-2 rounded-full bg-amber-500',
+  processing: 'h-2 w-2 rounded-full bg-amber-500',
+}
+
+export function OrderStatus({ status }: IOrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span className={orderStatusStyleMap[status]}></span>
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
